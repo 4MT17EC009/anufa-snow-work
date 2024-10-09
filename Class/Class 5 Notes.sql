6422806b136fa57d1d63cbf2fe1880b22a966ff8
4MT17EC009 <anufaraziyan2000@gmail.com>
2024-10-09 22:31:59 +0530

    Class 5 Notes Added

---
+++ Class/Class 5 Notes.sql
@@ -0,0 +1,37 @@
+>>JOINS IN SQL
+Joins are essential for combining data from related tables.
+Want to combine to get the Rows from 2 or more tables
+In order to Combine tables, we need a COMMON KEY
+The choice of join type affects the results returned, so it’s important to select the appropriate one based on your needs.
+
+>>TYPES OF JOINS IN SQL
+Inner Join
+Left Join
+Right join
+full join
+Self Join
+Cross Join
+
+>>Inner Join
+Fetch the matched values from both the tables
+
+>>Left Join
+Inner Join plus the values remaining in the left side of the table
+
+>>Right Join
+Inner Join plus the values remaining in the Right side of the table
+
+>>Full Join
+Left Join Values + Right Join Values
+
+>>Self Join
+Joins a table to itself. Useful for comparing rows within the same table.
+Rows Compared Within the Same Table
+
+>>Cross Join
+Returns the Cartesian product of two tables, meaning every row in the first table is combined with every row in the second table.
+All combinations of rows
+
+
+
+
