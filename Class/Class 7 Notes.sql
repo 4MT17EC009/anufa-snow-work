43d43db2d1149be365835bb4207932078fc4f80e
4MT17EC009 <anufaraziyan2000@gmail.com>
2024-10-19 08:57:56 +0530

    Class Notes 7 Added

---
+++ Class/Class 7 Notes.sql
@@ -0,0 +1,29 @@
+>> GIT and GITHUB
+Git and GitHub are often used together, but they serve different purposes. Here's a breakdown of each:
+
+--- GIT --
+>> VERSION CONTROL TOOL - Software 
+We are preparing SQL and keeping into local system, we need to place it at repositories. 
+Git is a distributed version control system that helps developers track changes to source code over time. It allows multiple people to work on the same project simultaneously without overwriting each other's work.
+Version Control: Keeps a history of changes, allowing you to revert to previous versions if needed.
+
+--- GIT HUB ---
+>> Code Repository
+>> One of the cloud based platform where we can store all programs, and if access is public anyone can see all the codes and scripts
+Ex - If a person A has joined a new organisation and in a week has completed 90% of task, rest of the work to be assigned to Person B or C, if all the work is in local system, it becomes difficult so, GITHUB
+-- If so much of work in on local machine, system might crash and you may loose data so on daily basis pushing of data into github is imp
+
+>> CODE REPOS ORGANIZATIONS Use
+GitHub - Account creation from Google
+BitBucket - Username/Pwd
+Azure Repos - Create a Microsoft Account
+
+code push:
+-----------------------------
+ 
+1)  right click on the repo name select git bash
+2)  git status
+3)  git add .       or git add <specific file name>
+4)  git status      --> everything it should be in green color
+5)  git commit -m "comments "
+6)  git push -u origin <branch name>
